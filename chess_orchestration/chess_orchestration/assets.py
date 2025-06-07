beb1c0d897b7df773687092901c723bf2c748f85
Gabriel LE GALL <gabrielgall@hotmail.fr>
2025-06-07 19:54:58 +0200

    added files

--- chess_orchestration/chess_orchestration/assets.py
+++ chess_orchestration/chess_orchestration/assets.py
@@ -1,12 +1,20 @@
-from dagster import asset
-
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from chess_pipeline import run_chess_pipeline
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))) #C:\Users\User\dlt_sandbox
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'chess_pipeline'))) #C:\Users\User\dlt_sandbox\chess_pipeline
+from chess_pipeline import chess_pipeline_integration
+from dagster import asset
 
 @asset
 def chess_dlt_asset():
-    return run_chess_pipeline()
+    return chess_pipeline_integration.run_pipeline()
 
-chess_dlt_asset() # To remove once fixed
+chess_dlt_asset()
+
+# https://docs.dagster.io/integrations/libraries/dlt
+# https://dlthub.com/blog/dlt-dagster
+# https://pipeline2insights.substack.com/p/data-ingestion-with-dlt-bluesky-to-s3-on-dagster
+# https://github.com/dlt-hub/dlt-dagster-demo/tree/main/github-issues/github_issues
+# https://dlthub.com/docs/general-usage/credentials/setup#secretstoml-and-configtoml
+# https://dlthub.com/docs/walkthroughs/deploy-a-pipeline/deploy-with-dagster
+# https://github.com/dlt-hub/dlthub-education/blob/main/workshops/workshop_august_2024/part2/deployment/deploy_dagster/README.md
